e9830db5eb01d2666f081e4828d91874e416a53f
Jeckonia Kwasa <inctifra@gmail.com>
2024-05-13 21:19:34 +0300

    update the email login sent link

--- amiribd/apis/main.py
+++ amiribd/apis/main.py
@@ -1,45 +1,30 @@
 if __name__ == "__main__":
     import requests
 
-
     headers = {
-
-    'Content-Type': 'application/json',
-
-    'Authorization': 'Bearer VY6FaJv7CPx9yVLe5Pk507CIh5OK'
-
+        "Content-Type": "application/json",
+        "Authorization": "Bearer VY6FaJv7CPx9yVLe5Pk507CIh5OK",
     }
-
 
     payload = {
-
         "BusinessShortCode": 174379,
-
         "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQwNTEzMTUyMTUz",
-
         "Timestamp": "20240513152153",
-
         "TransactionType": "CustomerPayBillOnline",
-
         "Amount": 1,
-
         "PartyA": 254745547755,
-
         "PartyB": 174379,
-
         "PhoneNumber": 254745547755,
-
         "CallBackURL": "https://liquid-investment-c119b5140660.herokuapp.com/apis/mpesa/callback",
-
         "AccountReference": "SynckPay",
-
-        "TransactionDesc": "Buy Mitumba" 
-
+        "TransactionDesc": "Buy Mitumba",
     }
 
+    response = requests.request(
+        "POST",
+        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+        headers=headers,
+        json=payload,
+    )
 
-    response = requests.request("POST", 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', headers = headers, data = payload)
-
-    print(response.text.encode('utf8'))
-
-    
+    print(response.text.encode("utf8"))
