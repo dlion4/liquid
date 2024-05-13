d46b3a204986c1ccb279a918e97eb20c3406a149
Jeckonia Kwasa <inctifra@gmail.com>
2024-05-13 14:55:28 +0300

    create a simulator mpesa callback url fro testing

--- amiribd/apis/views.py
+++ amiribd/apis/views.py
@@ -1,8 +1,17 @@
+import json
 from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-def mpesa_callback_url(request):
-    return JsonResponse({"message":"Mpesa callback url"})
 
+def mpesa_callback_url(request):
+    payload: str | bytearray | bytes = request.body or "request body data"
+
+    return JsonResponse(
+        {
+            "message": "Mpesa callback url",
+            "status": 200,
+            "data": {"url": "https://mpesa.com", "payload": json.loads(payload)},
+        }
+    )
