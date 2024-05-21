c4c43e8b27fc8da8ea2668de74e9e13c29348e1f
Jeckonia Kwasa <inctifra@gmail.com>
2024-05-21 07:46:48 +0300

    rate limit ai fix

--- amiribd/payments/admin.py
+++ amiribd/payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Payment
 
 # Register your models here.
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "profile", "channel", "account", "created_at", "updated_at")
