8ec8728f6c56065b1bb3f549d3f0b38c70929489
dlion4 <dantemilimo@gmail.com>
2024-10-17 13:39:46 +0300

    updated

--- amiribd/users/tasks.py
+++ amiribd/users/tasks.py
@@ -1,19 +1,12 @@
-import contextlib
 import logging
-import os
-from datetime import timedelta
 from typing import Any
-
-import resend
+import requests
 from celery import shared_task
 from django.contrib.auth.models import User as UserObject
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-from django.utils import timezone
 from django.utils.html import strip_tags
-
-from config.settings.base import resend
-
+from django.conf import settings
 
 
 @shared_task()
@@ -22,8 +15,9 @@ def get_users_count():
     return UserObject.objects.count()
 
 
-
 logger = logging.getLogger(__name__)
+
+EXPECTED_MAILGUN_STATUS_CODE = 200
 
 @shared_task
 def send_background_email(
@@ -32,7 +26,7 @@ def send_background_email(
     context: dict[str, Any] | None = None,
     recipients: list[str] | list | None = None,
     ):
-        """
+    """
         A Celery task that sends emails.
 
         This function is a Celery task that is used to send emails. It does not take any
@@ -45,49 +39,53 @@ def send_background_email(
         returns without doing anything.
 
         """
-        if recipients is None:
-            recipients = []
-        if context is None:
-            context = {}
-        if context is not None:
-            context.update({
+    if recipients is None:
+        recipients = []
+    if context is None:
+        context = {}
+    if context is not None:
+        context.update({
                 "team": "Earnkraft",
             })
 
-        # try:
+    try:
         html_message = render_to_string(template_name, context)
-        #     plain_message = strip_tags(html_message)
+        from_email = settings.DEFAULT_FROM_EMAIL
         subject = context.get("subject", "[Earnkraft Investment] Successful onboarding")
-        #     from_email = context.get("from_email", sender.email if sender else None)
         to = recipients or [context.get("email")]
 
-        #     logger.info(f'Sending email from {from_email} to {to} with subject "{subject}"')  # noqa: E501, G004
+        logger.info(f'Sending email from {from_email} to {to} with subject "{subject}"')  # noqa: E501, G004
+        response = send_email_with_attachment(
+            "",
+            "Earnkraft <postmaster@earnkraft.com>",
+            to,
+            subject,
+            html_message,
+        )
+        response.raise_for_status()
+        if response.status_code == EXPECTED_MAILGUN_STATUS_CODE:
+            logger.info(f"Email sent successfully to {to}")  # noqa: E501, G004
+        else:
+            logger.error(f"Failed to send email to {to}. Error: {response.text}")  # noqa: E501, G004
+    except Exception as e:
+        logger.exception(str(e))
 
-        #     message = EmailMultiAlternatives(
-        #         subject=subject,
-        #         body=plain_message,
-        #         from_email=from_email,
-        #         to=to,
-        #     )
 
-        #     message.attach_alternative(html_message, "text/html")
-        #     message.send()
-            # Now, using resend API
-        params: resend.Emails.SendParams = {
-            "from": "Earnkraft <email@earnkraft.com>",
+def send_email_with_attachment(
+    mailgun_api_key:str,
+    default_from_email:str,
+    to:list[str],
+    subject:str, html_content: str):
+    response = requests.post(
+        "https://api.mailgun.net/v3/earnkraft.com/messages",
+        auth=("api", mailgun_api_key),
+        data={
+            "from": default_from_email,
             "to": to,
             "subject": subject,
-            "html": html_message,
-        }
-
-        email = resend.Emails.send(params)
-        print(email)
-        logger.info(f"Email successfully sent via Resend API to {to}")
-
-        #     logger.info(f"Email successfully sent to {to}")  # noqa: G004
-        # except Exception as e:
-        #     logger.exception(e)
-
-
-
-
+            "html": html_content,
+        },
+        timeout=50,
+    )
+    response.raise_for_status()
+    return response
